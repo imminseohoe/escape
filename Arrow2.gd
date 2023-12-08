781237cd2db88c95e191da9c6221b4c913b15d9b
imminseohoe <imminseohoe@gmail.com>
2023-12-09 02:04:35 +0700

    first

---
+++ Arrow2.gd
@@ -0,0 +1,11 @@
+extends TextureRect
+
+func _ready():
+	set_process_input(true)
+
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		get_tree().change_scene("res://main1.tscn")
---
+++ main.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	pass 
+
+func _process(delta):
+	pass
+
+func _on_button_pressed():
+	get_tree().change_scene("res://main1.tscn")
