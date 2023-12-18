c891491e5c8a0edbe9595fefb18745891bbde004
imminseohoe <imminseohoe@gmail.com>
2023-12-19 00:21:06 +0700

    git이 커밋하래요

--- main_script/main_1.gd
+++ main_script/main_1.gd
@@ -14,3 +14,7 @@ func _on_left_button_pressed():
 	get_tree().change_scene_to_file("res://main_scene/main_4.tscn")
 
 
+
+
+func _on_door_lock_pressed():
+	get_tree().change_scene_to_file("res://main_scene/door_close.tscn")
